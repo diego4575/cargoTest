242e793be45e742375245c83443b426f840f86fa
Diego Rochin <drochin@mx1.ibm.com>
2020-02-25 17:26:32 -0600

    up all

---
+++ app/src/main/java/com/diegorochin/movies/api/Service.java
@@ -0,0 +1,16 @@
+package com.diegorochin.movies.api;
+
+import com.diegorochin.movies.model.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface Service {
+    @GET("movie/popular")
+    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+}
